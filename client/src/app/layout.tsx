02a7b407d85495b8633f53c7f77003f8b48b0e6a
nikapadia <30702376+nikapadia@users.noreply.github.com>
2024-03-30 03:08:39 -0400

    Next files removed or replaced
    Routing and layout for navbar are still broken however

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,42 +1,26 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import React from 'react';
 import { ThemeProvider } from "@/components/theme-provider";
-import { getServerSession } from "next-auth";
-import SessionProvider from "@/components/session-provider";
 import Navbar from "@/components/navbar";
 
 import "./globals.css";
-const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-	title: "PolyCal",
-	description: "A calendar for RPI students",
-};
-
-export default async function RootLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
-    const session = await getServerSession();
-	return (
-		<html lang="en">
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
             <head>
                 <link rel="icon" href="" />
             </head>
-			<body className={inter.className}>
-                <SessionProvider session={session}>
-                    <ThemeProvider
-                        attribute="class"
-                        defaultTheme="system"
-                        enableSystem
-                        disableTransitionOnChange
-                    >
-                        <Navbar />
-                        {children}
-                    </ThemeProvider>
-                </SessionProvider>
-			</body>
-		</html>
-	);
+            <body>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    <Navbar />
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
 }
---
+++ client/src/main.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Page from "./app/page";
+import "./app/globals.css";
+
+ReactDOM.render(<Page />, document.getElementById("root"));
